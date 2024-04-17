97f3e4a6788e34bb52dbf614b10dca9695cc4bae
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 03:02:09 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/service/WebBlogService.java
+++ src/main/java/rufethuseynov/blogapp/service/WebBlogService.java
@@ -1,8 +1,11 @@
 package rufethuseynov.blogapp.service;
 
+import rufethuseynov.blogapp.dto.dto.EntityByIdDto;
 import rufethuseynov.blogapp.dto.response.BlogPageResponse;
+import rufethuseynov.blogapp.dto.response.WebBlogReadByIdResponse;
 
 public interface WebBlogService {
 
     BlogPageResponse getAllBlogs(int page, int count);
+    WebBlogReadByIdResponse getBlogById(EntityByIdDto dto);
 }
