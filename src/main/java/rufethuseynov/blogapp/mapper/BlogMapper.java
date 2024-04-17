ef986ae1feaf4fd3c8749e1fe4128b25e4d270b8
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 03:47:37 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/mapper/BlogMapper.java
+++ src/main/java/rufethuseynov/blogapp/mapper/BlogMapper.java
@@ -4,10 +4,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import rufethuseynov.blogapp.dto.dto.BlogPageDto;
+import rufethuseynov.blogapp.dto.dto.WebBlogReadDto;
 import rufethuseynov.blogapp.entity.BlogEntity;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BlogMapper {
 
     BlogPageDto blogPageDto(BlogEntity entity);
+    WebBlogReadDto toWebBlogReadDto(BlogEntity entity);
 }
