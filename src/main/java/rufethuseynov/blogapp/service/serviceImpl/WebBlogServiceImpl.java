754ac9e30765e96efebb5604bb5d29563fae6efc
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 03:47:42 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/service/serviceImpl/WebBlogServiceImpl.java
+++ src/main/java/rufethuseynov/blogapp/service/serviceImpl/WebBlogServiceImpl.java
@@ -9,12 +9,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import rufethuseynov.blogapp.dto.dto.EntityByIdDto;
+import rufethuseynov.blogapp.dto.dto.WebBlogReadDto;
 import rufethuseynov.blogapp.dto.response.BlogPageResponse;
 import rufethuseynov.blogapp.dto.response.WebBlogReadByIdResponse;
 import rufethuseynov.blogapp.entity.BlogEntity;
+import rufethuseynov.blogapp.entity.ImageEntity;
 import rufethuseynov.blogapp.mapper.BlogMapper;
+import rufethuseynov.blogapp.mapper.ImageMapper;
 import rufethuseynov.blogapp.repository.BlogRepository;
+import rufethuseynov.blogapp.repository.ImageRepository;
 import rufethuseynov.blogapp.service.WebBlogService;
+
+import java.util.List;
 
 @Service
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -23,6 +29,8 @@ public class WebBlogServiceImpl implements WebBlogService {
 
     BlogRepository blogRepository;
     BlogMapper blogMapper;
+    ImageRepository imageRepository;
+    ImageMapper imageMapper;
 
     @Override
     public BlogPageResponse getAllBlogs(int page, int count) {
@@ -38,7 +46,13 @@ public class WebBlogServiceImpl implements WebBlogService {
 
     @Override
     public WebBlogReadByIdResponse getBlogById(EntityByIdDto dto) {
+        BlogEntity blogEntity = blogRepository.findById(dto.getId()).get();
+        List<ImageEntity> imageList = imageRepository.findByFkBlogId(blogEntity.getId());
 
-        return null;
+        return WebBlogReadByIdResponse
+                .builder()
+                .webBlogReadDto(blogMapper.toWebBlogReadDto(blogEntity))
+                .webBlogReadByIdDtoList(imageList.stream().map(imageMapper::toWebBlogReadIdDto).toList())
+                .build();
     }
 }
