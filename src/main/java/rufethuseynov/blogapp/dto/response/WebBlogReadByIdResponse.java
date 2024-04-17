6b954db959b9243f70c41b78d95fad6df2aa07c7
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 03:02:14 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/dto/response/WebBlogReadByIdResponse.java
@@ -0,0 +1,14 @@
+package rufethuseynov.blogapp.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@Builder
+public class WebBlogReadByIdResponse {
+
+
+}
