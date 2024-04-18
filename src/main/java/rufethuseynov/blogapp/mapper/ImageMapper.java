fdea10c743a92bb1bfc45f10b4b92456b4ca93e5
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 02:13:08 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/mapper/ImageMapper.java
+++ src/main/java/rufethuseynov/blogapp/mapper/ImageMapper.java
@@ -3,6 +3,7 @@ package rufethuseynov.blogapp.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import rufethuseynov.blogapp.dto.dto.AdminImageReadDto;
 import rufethuseynov.blogapp.dto.dto.ImageSaveDto;
 import rufethuseynov.blogapp.dto.dto.WebBlogReadImageDto;
 import rufethuseynov.blogapp.entity.ImageEntity;
@@ -12,4 +13,5 @@ public interface ImageMapper {
 
     WebBlogReadImageDto toWebBlogReadIdDto(ImageEntity imageEntity);
     ImageEntity toImageEntity(ImageSaveDto imageSaveDto);
+    AdminImageReadDto toAdminImageReadDto(ImageEntity entity);
 }
