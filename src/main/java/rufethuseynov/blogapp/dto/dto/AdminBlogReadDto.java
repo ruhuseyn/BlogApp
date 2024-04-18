87e17dbd9cd3557bf074c3202891ebe0b2b75d29
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 02:13:26 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/dto/dto/AdminBlogReadDto.java
@@ -0,0 +1,19 @@
+package rufethuseynov.blogapp.dto.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@Builder
+public class AdminBlogReadDto {
+
+    String name;
+    String title;
+    String content;
+    Long publishDate;
+    Long viewCount;
+    String coverImageUrl;
+}
