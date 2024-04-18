8a34b4ab451db9ab9d43581ccd619ca774be2f6d
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 02:13:05 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/service/serviceImpl/admin/AdminBlogDeleteService.java
+++ src/main/java/rufethuseynov/blogapp/service/serviceImpl/admin/AdminBlogDeleteService.java
@@ -5,10 +5,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 import rufethuseynov.blogapp.dto.dto.EntityByIdDto;
+import rufethuseynov.blogapp.entity.BlogEntity;
+import rufethuseynov.blogapp.entity.ImageEntity;
+import rufethuseynov.blogapp.exception.subexceptions.BlogNotFoundException;
 import rufethuseynov.blogapp.mapper.BlogMapper;
 import rufethuseynov.blogapp.mapper.ImageMapper;
 import rufethuseynov.blogapp.repository.BlogRepository;
 import rufethuseynov.blogapp.repository.ImageRepository;
+
+import java.util.List;
 
 @Service
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -17,10 +22,17 @@ public class AdminBlogDeleteService {
 
     BlogRepository blogRepository;
     ImageRepository imageRepository;
-    BlogMapper blogMapper;
-    ImageMapper imageMapper;
 
     public void deleteBlog(EntityByIdDto dto){
+        BlogEntity entity = blogRepository.findById(dto.getId())
+                .orElseThrow(()->new BlogNotFoundException("Blog is not found"));
+        entity.setStatus("D");
+        blogRepository.save(entity);
 
+        List<ImageEntity> imageEntityList = imageRepository.findByFkBlogId(dto.getId());
+        for(ImageEntity image: imageEntityList){
+            image.setStatus("D");
+            imageRepository.save(image);
+        }
     }
 }
