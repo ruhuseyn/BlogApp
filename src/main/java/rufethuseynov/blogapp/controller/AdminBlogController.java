f02008ee36476aa572a94c1817122450130222e8
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 00:00:22 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/controller/AdminBlogController.java
+++ src/main/java/rufethuseynov/blogapp/controller/AdminBlogController.java
@@ -1,4 +1,18 @@
 package rufethuseynov.blogapp.controller;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequestMapping("/api/v1/web")
 public class AdminBlogController {
+
+
 }
