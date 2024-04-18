163d1963d380981ae59d144dad4837368a614011
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 02:13:22 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/service/serviceImpl/admin/AdminBlogReadService.java
@@ -0,0 +1,43 @@
+package rufethuseynov.blogapp.service.serviceImpl.admin;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import rufethuseynov.blogapp.dto.dto.AdminBlogReadDto;
+import rufethuseynov.blogapp.dto.dto.AdminReadByStatusDto;
+import rufethuseynov.blogapp.dto.response.AdminBlogReadResponse;
+import rufethuseynov.blogapp.entity.BlogEntity;
+import rufethuseynov.blogapp.entity.ImageEntity;
+import rufethuseynov.blogapp.mapper.BlogMapper;
+import rufethuseynov.blogapp.mapper.ImageMapper;
+import rufethuseynov.blogapp.repository.BlogRepository;
+import rufethuseynov.blogapp.repository.ImageRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class AdminBlogReadService {
+    BlogRepository blogRepository;
+    ImageRepository imageRepository;
+    BlogMapper blogMapper;
+    ImageMapper imageMapper;
+
+    public List<AdminBlogReadResponse> readAll(AdminReadByStatusDto dto){
+        List<AdminBlogReadResponse> response = new ArrayList<>();
+        List<BlogEntity> blogEntityList = blogRepository.findAllByStatus(dto.getStatus());
+        for(BlogEntity entity: blogEntityList){
+            List<ImageEntity> imageEntityList = imageRepository.findByFkBlogId(entity.getId());
+            AdminBlogReadResponse adminBlogReadResponse = AdminBlogReadResponse
+                    .builder()
+                    .adminBlogReadDto(blogMapper.toAdminBlogReadDto(entity))
+                    .adminImageReadDtoList(imageEntityList.stream().map(imageMapper::toAdminImageReadDto).toList())
+                    .build();
+            response.add(adminBlogReadResponse);
+        }
+        return response;
+    }
+}
