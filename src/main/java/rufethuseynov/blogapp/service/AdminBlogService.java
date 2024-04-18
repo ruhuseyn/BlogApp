4d0e7afa4aed9f812552cbe85f411fddc79ff5dd
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 22:33:36 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/service/AdminBlogService.java
+++ src/main/java/rufethuseynov/blogapp/service/AdminBlogService.java
@@ -1,4 +1,9 @@
 package rufethuseynov.blogapp.service;
 
+import rufethuseynov.blogapp.dto.dto.CreateResponseDto;
+import rufethuseynov.blogapp.dto.request.BlogSaveRequestDto;
+
 public interface AdminBlogService {
+
+    CreateResponseDto save(BlogSaveRequestDto dto);
 }
