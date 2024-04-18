8a5970c60c95e80d1893a3e92d8bc3e5febed59b
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 01:31:48 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/exception/CustomizedExceptionHandler.java
@@ -0,0 +1,24 @@
+package rufethuseynov.blogapp.exception;
+
+import rufethuseynov.blogapp.exception.superexeptions.MainNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(MainNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
