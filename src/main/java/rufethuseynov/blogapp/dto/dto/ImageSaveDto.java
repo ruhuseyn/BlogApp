d97b1a2508702878f58f1b6780760b2267e7e2b9
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 22:36:20 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/dto/dto/ImageSaveDto.java
@@ -0,0 +1,12 @@
+package rufethuseynov.blogapp.dto.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@Builder
+public class ImageSaveDto {
+}
