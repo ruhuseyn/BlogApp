670011e9006fe2c68c2ae9ed0a853bd415ebdfff
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 02:13:11 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/repository/BlogRepository.java
+++ src/main/java/rufethuseynov/blogapp/repository/BlogRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rufethuseynov.blogapp.entity.BlogEntity;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface BlogRepository extends JpaRepository<BlogEntity, String> {
+    List<BlogEntity> findAllByStatus(String a);
 }
