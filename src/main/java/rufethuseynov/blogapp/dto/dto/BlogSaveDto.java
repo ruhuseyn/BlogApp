a40ec9d3dab5bfa281735a4b0f4c4425fb29ae97
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 22:34:15 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/dto/dto/BlogSaveDto.java
@@ -0,0 +1,11 @@
+package rufethuseynov.blogapp.dto.dto;
+
+import jakarta.persistence.Column;
+
+public class BlogSaveDto {
+
+    String name;
+    String title;
+    String content;
+    Long publishDate;
+}
