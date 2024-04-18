e4c86d6d597bde33e71dcc5b974e9a0a55d40acf
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 02:13:29 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/controller/AdminBlogController.java
+++ src/main/java/rufethuseynov/blogapp/controller/AdminBlogController.java
@@ -6,11 +6,16 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import rufethuseynov.blogapp.dto.dto.AdminReadByStatusDto;
 import rufethuseynov.blogapp.dto.dto.CreateResponseDto;
 import rufethuseynov.blogapp.dto.dto.EntityByIdDto;
 import rufethuseynov.blogapp.dto.request.BlogSaveRequestDto;
+import rufethuseynov.blogapp.dto.response.AdminBlogReadResponse;
 import rufethuseynov.blogapp.service.serviceImpl.admin.AdminBlogDeleteService;
+import rufethuseynov.blogapp.service.serviceImpl.admin.AdminBlogReadService;
 import rufethuseynov.blogapp.service.serviceImpl.admin.AdminBlogSaveService;
+
+import java.util.List;
 
 @Validated
 @RestController
@@ -21,6 +26,7 @@ public class AdminBlogController {
 
     AdminBlogSaveService adminBlogSaveService;
     AdminBlogDeleteService adminBlogDeleteService;
+    AdminBlogReadService adminBlogReadService;
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
@@ -34,5 +40,9 @@ public class AdminBlogController {
         adminBlogDeleteService.deleteBlog(dto);
     }
 
-
+    @PostMapping("/read-all")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<AdminBlogReadResponse> readAll(@RequestBody AdminReadByStatusDto dto){
+        return adminBlogReadService.readAll(dto);
+    }
 }
