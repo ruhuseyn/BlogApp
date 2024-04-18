38abda71433b89d0869bb43744123b91495af0d3
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-18 22:36:17 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/dto/dto/BlogSaveDto.java
+++ src/main/java/rufethuseynov/blogapp/dto/dto/BlogSaveDto.java
@@ -1,7 +1,14 @@
 package rufethuseynov.blogapp.dto.dto;
 
-import jakarta.persistence.Column;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@Builder
 public class BlogSaveDto {
 
     String name;
