b5de88e4dce6b4494d507f963f989a1d5bf1356f
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 01:31:42 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/exception/superexeptions/MainNotFoundException.java
@@ -0,0 +1,7 @@
+package rufethuseynov.blogapp.exception.superexeptions;
+
+public class MainNotFoundException extends RuntimeException{
+    public MainNotFoundException(String message) {
+        super(message);
+    }
+}
