0fc2c01cb85e98a5f79f9130bdff72358dbcca80
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 23:36:30 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/entity/BlogEntity.java
+++ src/main/java/rufethuseynov/blogapp/entity/BlogEntity.java
@@ -2,9 +2,11 @@ package rufethuseynov.blogapp.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import rufethuseynov.blogapp.utility.CoreEntity;
 
@@ -12,6 +14,7 @@ import rufethuseynov.blogapp.utility.CoreEntity;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity(name = "blogs")
 public class BlogEntity extends CoreEntity {
 
