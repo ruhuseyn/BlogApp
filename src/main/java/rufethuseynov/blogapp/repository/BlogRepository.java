d7c949b802c79a5edd083276295edf3674e65810
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 23:36:27 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/repository/BlogRepository.java
+++ src/main/java/rufethuseynov/blogapp/repository/BlogRepository.java
@@ -2,15 +2,15 @@ package rufethuseynov.blogapp.repository;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import rufethuseynov.blogapp.entity.BlogEntity;
-
 import java.util.List;
 
 @Repository
 @Transactional
-public interface BlogRepository extends JpaRepository<BlogEntity, String> {
+public interface BlogRepository extends JpaRepository<BlogEntity, String>, JpaSpecificationExecutor<BlogEntity> {
+
     List<BlogEntity> findAllByStatus(String a);
-    List<BlogEntity> findByNameContainingIgnoreCaseAndPublishDateBetween(String nameKeyword, Long minPublishDate, Long maxPublishDate);
 
 }
