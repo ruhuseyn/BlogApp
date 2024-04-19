9a357b76afbe71d3f1c7230e449eb96b8ff710f8
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-19 22:39:27 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/service/web/WebBlogReadService.java
+++ src/main/java/rufethuseynov/blogapp/service/web/WebBlogReadService.java
@@ -1,13 +1,10 @@
-package rufethuseynov.blogapp.service.serviceImpl;
+package rufethuseynov.blogapp.service.web;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import rufethuseynov.blogapp.dto.dto.EntityByIdDto;
-import rufethuseynov.blogapp.dto.response.BlogPageResponse;
 import rufethuseynov.blogapp.dto.response.WebBlogReadByIdResponse;
 import rufethuseynov.blogapp.entity.BlogEntity;
 import rufethuseynov.blogapp.entity.ImageEntity;
@@ -21,25 +18,12 @@ import java.util.List;
 @Service
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @RequiredArgsConstructor
-public class WebBlogServiceImpl{
+public class WebBlogReadService {
 
     BlogRepository blogRepository;
     BlogMapper blogMapper;
     ImageRepository imageRepository;
     ImageMapper imageMapper;
-
-
-    public BlogPageResponse getAllBlogs(int page, int count) {
-        Page<BlogEntity> userPage = blogRepository.findAll(PageRequest.of(page, count));
-
-        return new BlogPageResponse(
-                userPage.getContent().stream().map(blogMapper::blogPageDto).toList(),
-                userPage.getTotalElements(),
-                userPage.getTotalPages(),
-                userPage.hasNext()
-        );
-    }
-
 
     public WebBlogReadByIdResponse getBlogById(EntityByIdDto dto) {
         BlogEntity blogEntity = blogRepository.findById(dto.getId()).get();
