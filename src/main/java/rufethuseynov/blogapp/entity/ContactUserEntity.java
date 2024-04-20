1358a0cda3b47d2699523a224ca665c8bc8e1bbd
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-20 14:12:30 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/entity/ContactUserEntity.java
@@ -0,0 +1,26 @@
+package rufethuseynov.blogapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import rufethuseynov.blogapp.utility.CoreEntity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "blogs")
+public class ContactUserEntity extends CoreEntity {
+
+    String name;
+    @Column(name = "phone_number")
+    String phoneNumber;
+    String email;
+    String message;
+}
