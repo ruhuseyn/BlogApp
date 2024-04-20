a264b8ce731add3b50685d30565eea66dcb4bbb1
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-20 14:12:44 +0400

    changes

---
+++ src/main/java/rufethuseynov/blogapp/mapper/ContactUserMapper.java
@@ -0,0 +1,13 @@
+package rufethuseynov.blogapp.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import rufethuseynov.blogapp.dto.request.ContactUserSaveRequestDto;
+import rufethuseynov.blogapp.entity.ContactUserEntity;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ContactUserMapper {
+
+    ContactUserEntity toContactUserEntity(ContactUserSaveRequestDto dto);
+}
