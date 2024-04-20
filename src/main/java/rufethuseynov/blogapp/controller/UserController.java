e50399977721ee12565f0f3ab7e7360c943b76fb
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-20 14:21:06 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/controller/UserController.java
+++ src/main/java/rufethuseynov/blogapp/controller/UserController.java
@@ -4,8 +4,6 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import rufethuseynov.blogapp.dto.request.UserUpdateRequestDto;
