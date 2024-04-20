304fe55caaac1578c2966ace4fa3f88acf731e85
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-20 14:19:02 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/repository/ContactUserRepository.java
+++ src/main/java/rufethuseynov/blogapp/repository/ContactUserRepository.java
@@ -1,9 +1,11 @@
 package rufethuseynov.blogapp.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rufethuseynov.blogapp.entity.ContactUserEntity;
 
 @Repository
+@Transactional
 public interface ContactUserRepository extends JpaRepository<ContactUserEntity, String> {
 }
