fd7804b5b2443afc85010d7bcdf332cd7a7414c0
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-04-20 14:00:27 +0400

    changes

--- src/main/java/rufethuseynov/blogapp/controller/UserController.java
+++ src/main/java/rufethuseynov/blogapp/controller/UserController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import rufethuseynov.blogapp.dto.request.UserUpdateRequestDto;
 import rufethuseynov.blogapp.service.admin.UserService;
 import rufethuseynov.blogapp.dto.request.RegisterRequest;
 
@@ -26,8 +27,15 @@ public class UserController {
     }
 
     @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
     public String login(@RequestBody @Valid RegisterRequest request) {
         return userService.login(request);
+    }
+
+    @PostMapping("/update-profile")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateProfile(@RequestBody UserUpdateRequestDto dto){
+        userService.updateProfile(dto);
     }
 
 }
